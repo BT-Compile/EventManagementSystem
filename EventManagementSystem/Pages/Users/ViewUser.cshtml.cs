2ae0cff101757e4762ab37fbebae51344e758002
YellowBlvck <121653356+BT-Compile@users.noreply.github.com>
2023-11-21 17:28:40 -0500

    pee pee poo poo

--- EventManagementSystem/Pages/Users/ViewUser.cshtml.cs
+++ EventManagementSystem/Pages/Users/ViewUser.cshtml.cs
@@ -1,7 +1,10 @@
+using EventManagementSystem.Pages.DataClasses;
 using EventManagementSystem.Pages.DB;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 using System.Data.SqlClient;
+using System.Xml.Linq;
 
 namespace EventManagementSystem.Pages.Users
 {
@@ -21,13 +24,9 @@ namespace EventManagementSystem.Pages.Users
         {
             UserName = DBClass.GetUserName(userID);
 
-            string sqlQuery = "SELECT [User].FirstName, [User].LastName, [User].Email, [User].PhoneNumber, [User].Username, [User].Accomodation, UserEvent.EventName, Activity.ActivityName, Activity.Date, Building.Name, Room.RoomNumber " +
-                                "FROM Activity INNER JOIN ActivityAttendance ON Activity.ActivityID = ActivityAttendance.ActivityID INNER JOIN " +
-                                "Event ON Activity.EventID = Event.EventID INNER JOIN Building ON Event.BuildingID = Building.BuildingID INNER JOIN " +
-                                "EventAttendance ON Event.EventID = EventAttendance.EventID INNER JOIN [User] ON ActivityAttendance.UserID = " +
-                                "[User].UserID AND EventAttendance.UserID = [User].UserID INNER JOIN Room ON Activity.RoomID = Room.RoomID AND Building.BuildingID = Room.BuildingID " +
-                                "WHERE Attendance.UserID = " + userID +
-                                " ORDER BY Activity.Date;";
+            // JON USE THE DATACLASS
+            string sqlQuery = "";
+
 
             SqlDataReader UserViewer = DBClass.GeneralReaderQuery(sqlQuery);
 
@@ -42,7 +41,6 @@ namespace EventManagementSystem.Pages.Users
                     Username = UserViewer["Username"].ToString(),
                     Accomodation = UserViewer["Accomodation"].ToString(),
                     EventName = UserViewer["EventName"].ToString(),
-                    ActivityName = UserViewer["ActivityName"].ToString(),
                     Date = DateTime.Parse(UserViewer["Date"].ToString()),
                     BuildingName = UserViewer["Name"].ToString(),
                     RoomNumber = int.Parse(UserViewer["RoomNumber"].ToString())
