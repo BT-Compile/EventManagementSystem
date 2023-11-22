42bc57b2bf06d2365f8b5e9370d58abb40842997
Steven Roenker <roenkesm@dukes.jmu.edu>
2023-11-22 15:14:25 -0500

    (Admin) Location functionality implemented

--- EventManagementSystem/Pages/DataClasses/Event.cs
+++ EventManagementSystem/Pages/DataClasses/Event.cs
@@ -16,6 +16,7 @@
 
         public int? Capacity { get; set; }
 
+        public string? EventType { get; set;}
         public int UserID { get; set; }
 
         public string? Status { get; set; }
@@ -23,8 +24,6 @@
         public string? SpaceID { get; set; }
 
         public string? EventType { get; set;}
-
-        public string? SpaceName { get; set; }
 
         public string? SpaceAddress { get; set;}
 
