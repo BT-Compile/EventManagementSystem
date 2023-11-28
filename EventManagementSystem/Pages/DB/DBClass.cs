1f27f1efca7c2d80f78d0c0feaa9554fc17d1f01
Ben Trang <121653356+BT-Compile@users.noreply.github.com>
2023-11-28 12:26:53 -0500

    Ben fixed timeout issue

--- EventManagementSystem/Pages/DB/DBClass.cs
+++ EventManagementSystem/Pages/DB/DBClass.cs
@@ -12,9 +12,9 @@ namespace EventManagementSystem.Pages.DB
         public static SqlConnection DBConnection = new SqlConnection();
 
         // Connection String
-        public static readonly string CapstoneDBConnString = "Server=Localhost;Database=CAPSTONE;Trusted_Connection=True";
+        public static readonly string CapstoneDBConnString = "Server=Localhost;Database=CAPSTONE;Trusted_Connection=True;max pool size=30000";
         // For Hashed Passwords
-        private static readonly string? AuthDBConnString = "Server=Localhost;Database=AUTH;Trusted_Connection=True";
+        private static readonly string? AuthDBConnString = "Server=Localhost;Database=AUTH;Trusted_Connection=True;max pool size= 30000";
 
         // Can run and return results for any "ExecuteReader query", if results exist.
         // Query is passed from the invoking code.
