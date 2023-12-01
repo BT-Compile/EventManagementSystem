e1e965e2d88b6587ea31668020e998339119a9ed
Jonathan Lee <jlee4668@outlook.com>
2023-12-01 13:47:00 -0500

    asasas

--- EventManagementSystem/Pages/Teams/Index.cshtml.cs
+++ EventManagementSystem/Pages/Teams/Index.cshtml.cs
@@ -27,13 +27,7 @@ namespace EventManagementSystem.Pages.Teams
 
         public IActionResult OnGet()
         {
-            if (HttpContext.Session.GetString("RoleType") != "Admin" &&
-                (HttpContext.Session.GetString("RoleType") == "Presenter" || HttpContext.Session.GetString("RoleType") == "Judge"
-                || HttpContext.Session.GetString("RoleType") == "Participant" || HttpContext.Session.GetString("RoleType") == "Organizer"))
-            {
-                return RedirectToPage("/Attendee/Index");
-            }
-            else if (HttpContext.Session.GetString("RoleType") == null)
+            if (HttpContext.Session.GetString("RoleType") == null)
             {
                 return RedirectToPage("/Login/Index");
             }
